b13ed217929aaf05f1771cc463429802f0033e28
Alex Ganose <alexganose@googlemail.com>
2018-04-06 17:24:16 -0700

    TST: Reorganise tests directory

--- tests/tests_symmetry/test_seekpath_kpath.py
+++ tests/tests_symmetry/test_seekpath_kpath.py
@@ -6,11 +6,12 @@ from pymatgen.core.structure import Structure
 
 from vaspy.symmetry.seekpath_kpath import SeekpathKpath
 
+
 class SeekpathKpathTestCase(unittest.TestCase):
 
     def setUp(self):
         ge_poscar = pkg_resources.resource_filename(
-            __name__, path_join('Ge', 'POSCAR')
+            __name__, path_join('..', 'data', 'Ge', 'POSCAR')
         )
         self.ge_structure = Structure.from_file(ge_poscar)
 
@@ -22,11 +23,11 @@ class SeekpathKpathTestCase(unittest.TestCase):
                            'E': [1., 0., 1.], 'GAMMA': [0., 0., 0.]}
 
         kpath = SeekpathKpath.kpath_from_seekpath(in_path, in_point_coords)
-        self.assertEqual(kpath['path'], [['A', 'B', 'C'], ['D', '\Gamma']])
+        self.assertEqual(kpath['path'], [['A', 'B', 'C'], ['D', r'\Gamma']])
         self.assertEqual(kpath['kpoints'],
                          {'A': [1., 0., 0.], 'B': [0., 1., 0.],
                           'C': [0., 0., 1.], 'D': [0.5, 0.5, 0.],
-                          '\Gamma': [0., 0., 0.]})
+                          r'\Gamma': [0., 0., 0.]})
 
     def test_seekpath_kpath_load(self):
         """Check path generation from structure with seekpath"""
@@ -36,9 +37,11 @@ class SeekpathKpathTestCase(unittest.TestCase):
         self.assertEqual(kpath.spg_number, 227)
         self.assertEqual(kpath.kpoints['L'], [0.5, 0.5, 0.5])
         self.assertEqual(kpath.path,
-            [['\Gamma', 'X', 'U'], ['K', '\Gamma', 'L', 'W', 'X']])
+                         [[r'\Gamma', 'X', 'U'],
+                          ['K', r'\Gamma', 'L', 'W', 'X']])
         self.assertEqual(kpath.path_string,
-                         '\Gamma -> X -> U | K -> \Gamma -> L -> W -> X')
+                         r'\Gamma -> X -> U | K -> \Gamma -> L -> W -> X')
+
 
 if __name__ == '__main__':
     unittest.main()
