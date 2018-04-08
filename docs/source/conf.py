39f99c5e8c639475041ab31deeee7fc62d26f79b
Alex Ganose <alexganose@googlemail.com>
2018-04-07 23:44:25 -0700

    DOC: Add cli documentation for kgen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
---
+++ docs/source/tutorials.rst
@@ -0,0 +1,9 @@
+Tutorials
+=========
+
+The easiest way to use vaspy is through the command line interface.
+A broad range of functionality is available. See the individual pages
+for each command for more details:
+
+.. toctree::
+   vaspy-kgen
