8c33909a85b3686b17c728ec9ed244fc1e43127a
Alex Ganose <alexganose@googlemail.com>
2021-03-24 18:33:15 -0700

    Fix import order

--- sumo/symmetry/__init__.py
+++ sumo/symmetry/__init__.py
@@ -6,9 +6,9 @@
 Package containing functions for symmetry and k-point paths.
 """
 
+from sumo.symmetry.kpath import Kpath  # isort:skip
 from sumo.symmetry.brad_crack_kpath import BradCrackKpath
 from sumo.symmetry.custom_kpath import CustomKpath
-from sumo.symmetry.kpath import Kpath
 from sumo.symmetry.latimer_munro_kpath import LatimerKpath
 from sumo.symmetry.pymatgen_kpath import PymatgenKpath
 from sumo.symmetry.seekpath_kpath import SeekpathKpath
